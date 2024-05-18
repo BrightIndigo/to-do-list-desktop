491d57294c7e851a482e49ef103786aa42d4bf81
BrightIndigo <128691376+BrightIndigo@users.noreply.github.com>
2024-05-18 17:39:03 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import random
-from winotify import Notification, audio
 import time
-import playsound
+from winotify import Notification, audio
 import customtkinter as ctk
-import random
 
 def change_theme():
     if switch_var.get() == "on":
@@ -20,9 +17,7 @@ def alarm_start():
                              icon=r"D:\Dokumenty\Photos\ytlogo.png")
 
         toast.set_audio(audio.LoopingAlarm, loop=False)
-
         toast.add_actions(label="Click Me!", launch="https://www.python.org/")
-
         toast.show()
 
     def alarm(seconds):
@@ -36,21 +31,13 @@ def alarm_start():
             minutes_left = time_left // 60
             seconds_left = time_left % 60
 
-            if seconds_left < 10:
-                seconds_left = "0" + str(seconds_left)
-
-            if minutes_left < 10:
-                minutes_left = "0" + str(minutes_left)
-
-            print(f"{minutes_left}:{seconds_left}")
+            print(f"{minutes_left:02}:{seconds_left:02}")
 
         wiadomosc()
 
     if entry.get().isnumeric() and entry.get() != "":
         sec = int(entry.get())
         alarm(sec)
-
-
 
 app = ctk.CTk()
 app.geometry("1500x1080")
@@ -59,32 +46,26 @@ app.title("To do list desktop app")
 button = ctk.CTkButton(master=app, text="Start the countdown", command=alarm_start)
 button.place(relx=0.5, rely=0.15, anchor=ctk.CENTER)
 
-
 switch_var = ctk.StringVar(value="on")
 switch_1 = ctk.CTkSwitch(master=app, text="Theme", command=change_theme,
-                                   variable=switch_var, onvalue="on", offvalue="off")
+                         variable=switch_var, onvalue="on", offvalue="off")
 switch_1.pack(padx=20, pady=10)
 
 text_var = ctk.StringVar(value="Enter number of seconds to start alarm clock")
 
 label = ctk.CTkLabel(master=app,
-                               textvariable=text_var,
-                               width=120,
-                               height=25,
-                               corner_radius=8)
+                     textvariable=text_var,
+                     width=120,
+                     height=25,
+                     corner_radius=8)
 label.place(relx=0.5, rely=0.05, anchor=ctk.CENTER)
 
-
-
 entry = ctk.CTkEntry(master=app,
-                               placeholder_text="Enter number of seconds!",
-                               width=200,
-                               height=75,
-                               border_width=2,
-                               corner_radius=10)
+                     placeholder_text="Enter number of seconds!",
+                     width=200,
+                     height=75,
+                     border_width=2,
+                     corner_radius=10)
 entry.place(relx=0.5, rely=0.1, anchor=ctk.CENTER)
 
-
 app.mainloop()
-
-
